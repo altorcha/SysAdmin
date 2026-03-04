5989935844cacceae28deac4256979aef3f88e08
Alberto Torres <torreschaparroalberto91@gmail.com>
2026-03-03 22:21:17 -0700

    feat(FTP): Función para instalar el servicio FTP en Windows

--- Tarea 5 FTP/Scripts/Windows/servicio_ftp.ps1
+++ Tarea 5 FTP/Scripts/Windows/servicio_ftp.ps1
@@ -78,4 +78,62 @@ function Estado-FTP {
     }
 }
 
-Estado-FTP
+#Estado-FTP
+
+function Instalar-FTP {
+    Clear-Host
+    Write-host "==========================================================="
+    Write-Host "            INSTALACION DEL SERVICIO FTP"
+    Write-host "==========================================================="
+    Import-Module ServerManager
+
+    $FtpInstalado = Get-WindowsFeature -Name Web-FTP-Server
+
+    if ($FtpInstalado.InstallState -eq "Installed") {
+        Write-Host "El servicio FTP ya esta instalado." -ForegroundColor Yellow
+        Pause
+        return
+    }
+    Write-Host "Iniciando instalacion del servicio FTP..."
+    Install-WindowsFeature -Name Web-FTP-Server, Web-FTP-Ext, Web-FTP-Service, Web-Mgmt-Console | Out-Null
+
+    # Volvemos a validar si se instalo correctamente
+    $ftpCheck = Get-WindowsFeature -Name Web-FTP-Server
+
+    if ($ftpCheck.InstallState -eq "Installed") {
+
+        # Configurar el inicio automatico
+        Set-Service -Name ftpsvc -StartupType 'Automatic'
+
+        # Iniciar servicio FTP
+        Start-Service ftpsvc
+
+        
+        # Verificar y habilitar regla puerto 21
+        $fw21 = Get-NetFirewallRule -Name "FTP-Puerto21" -ErrorAction SilentlyContinue
+        if (!$fw21) {
+            New-NetFirewallRule -Name "FTP-Puerto21" -DisplayName "FTP (TCP 21)" -Protocol TCP -LocalPort 21 -Action Allow -Profile Any | Out-Null
+        }
+
+        # Verificar y habilitar regla puertos pasivos
+        $fwPasivo = Get-NetFirewallRule -Name "FTP-Pasivo" -ErrorAction SilentlyContinue
+        if (!$fwPasivo) {
+            New-NetFirewallRule -Name "FTP-Pasivo" -DisplayName "FTP Pasivo (40000-40100)" -Protocol TCP -LocalPort 40000-40100 -Action Allow -Profile Any | Out-Null
+        }
+
+        # Registrar el rango pasivo en la configuración de IIS
+        Import-Module WebAdministration
+        Set-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST' -filter "system.ftpServer/provider/options" -name "dataChannelPortRange" -value "40000-40100"
+
+        # Reiniciar para aplicar configuración pasiva
+        Restart-Service ftpsvc
+
+        Write-Host "Instalacion completada, servicio configurado en automatico y firewall asegurado." -ForegroundColor Green
+    }
+    else {
+        Write-Host "Error en la instalacion del Servidor FTP." -ForegroundColor Red
+    }
+
+    Pause
+}
+Instalar-FTP
