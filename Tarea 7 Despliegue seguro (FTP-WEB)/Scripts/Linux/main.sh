cd359055af926d79a60a891c94ae8dddd2f557bd
albertotorrescha <torreschaparroalberto91@gmail.com>
2026-03-22 00:10:11 -0700

    feat(FTP-WEB): Llamada a la función que instala los servidores web de forma online

--- Tarea 7 Despliegue seguro (FTP-WEB)/Scripts/Linux/main.sh
+++ Tarea 7 Despliegue seguro (FTP-WEB)/Scripts/Linux/main.sh
@@ -1,36 +1,30 @@
 #!/bin/bash
 source ./colores.sh
 source ./servicio_ftps.sh
-
-validar_root() {
-    if [ "$(id -u)" -ne 0 ]; then
-        echo -e "${NEGRITA}${FONDO_ROJO}${BLANCO_BRILLANTE} ERROR *** Este script debe ejecutarse como root ${RESET}"
-        exit 1
-    fi
-}
-
-Menu-Orquestador(){
+source ./servicio_http.sh
+source ./utilidades.sh
+Menu_Principal(){
     while true; do
         clear
         echo "=============================================================================="
         echo -e "${NEGRITA}${AZUL}           Despliegue de Servicios (FTP/WEB)          ${RESET}"
         echo "=============================================================================="
         echo "[1]   Menu FTP"
-        echo "[2]   Instalación de Servicios Web (Apache/Nginx/Tomcat)"
-        echo "[3]   Estado de Servicios Web"
+        echo "[2]   Instalación de Servidores Web (Apache/Nginx/Tomcat)"
+        echo "[3]   Estado de Servidores Web"
         echo "[4]   Salir"
         echo "==============================="
         read -p "Seleccione una opción: " opcion
 
         case $opcion in
             1)
-                menu-ftps
+                menu_ftps
                 ;;
             2)
-                menu-web
+                menu_instalacion_web
                 ;;
             3)
-                estado-web
+                status_srvweb
                 ;;
             4)
                 echo -e "${NEGRITA}${VERDE}Saliendo...${RESET}"
@@ -44,4 +38,4 @@ Menu-Orquestador(){
     done
 }
 validar_root
-Menu-Orquestador
+Menu_Principal
